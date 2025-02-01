8fd499bcab93a1d08a1891bad0b3025c79f98d57
rodi-314 <rodi314159@gmail.com>
2025-02-01 21:36:14 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Roderick Kong on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
