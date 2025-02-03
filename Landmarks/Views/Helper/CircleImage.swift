00b9fda301696a1679746a1972f31963998217b1
rodi-314 <rodi314159@gmail.com>
2025-02-04 01:10:24 +0800

    Second commit

---
+++ Landmarks/Views/Helper/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Roderick Kong on 2/2/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
